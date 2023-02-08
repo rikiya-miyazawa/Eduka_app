1c8d6d6fcf4eaef5f00bed6bbd1db8c1dda8386c
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-02-08 10:39:38 +0900

    feat:ルーティングをresourcesへ変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'educations/index'
-  get 'educations/new'
-  get 'educations/edit'
-  get 'educations/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :educations
 end
