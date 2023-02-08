bd6012a0357a6af0f7c7324f0ae458b2346d91dd
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-02-08 10:52:35 +0900

    feat:nameカラムにNot null制約を付与

--- db/migrate/20230208014307_create_educations.rb
+++ db/migrate/20230208014307_create_educations.rb
@@ -1,7 +1,7 @@
 class CreateEducations < ActiveRecord::Migration[6.1]
   def change
     create_table :educations do |t|
-      t.string :name
+      t.string :name, null:false
 
       t.timestamps
     end
