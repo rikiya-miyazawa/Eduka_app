a36ab92bd71fad4b0043af21c95c64af5f379bda
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-02-08 15:58:25 +0900

    feat:削除機能を実装

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -1,5 +1,6 @@
 class EducationsController < ApplicationController
-  bofore_action :set_education, only: %i(show edit update)
+  before_action :set_education, only: %i(show edit update destroy)
+
   def index
     @educations = Education.all
   end
@@ -33,6 +34,12 @@ class EducationsController < ApplicationController
   def show
   end
 
+  def destroy
+    @education.destroy
+    redirect_to educations_path,
+    notice: t('view.educations.notice.destroy_education')
+  end
+
   private
 
   def education_params
@@ -42,5 +49,5 @@ class EducationsController < ApplicationController
   def set_education
     @education = Education.find(params[:id])
   end
-  
+
 end
