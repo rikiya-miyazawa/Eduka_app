4a07720b1377e858ea47b3908450e3f1ba1bac8d
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-02-08 13:57:58 +0900

    feat:createアクション実装

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -6,9 +6,20 @@ class EducationsController < ApplicationController
     @education = Education.new
   end
 
+  def create
+    Education.create(education_params)
+    redirect_to educations_path
+  end
+
   def edit
   end
 
   def show
+  end
+
+  private
+
+  def education_params
+    params.require(:education).permit(:name)
   end
 end
