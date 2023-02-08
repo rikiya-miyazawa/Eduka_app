dd185e3a10c634aa43ce1192b1884d84d5ac0e24
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-02-08 17:31:16 +0900

    feat:Subjectモデルにバリデーションを設定

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,7 @@
 class Subject < ApplicationRecord
   belongs_to :education
+  validates :name, presence: true,
+                  length: { maximum: 100 }
+  #boolean型のpresence: true
+  validates :status, inclusion: {in: [true, false]}
 end
