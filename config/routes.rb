cf6d792b0b9660283ba1397d16a9074845d7baec
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-02-08 09:58:48 +0900

    add:教育ページを作成するため、controllerとviewファイルを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'educations/index'
+  get 'educations/new'
+  get 'educations/edit'
+  get 'educations/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
