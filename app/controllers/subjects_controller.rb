1fb3138561d99bd44e48618e50949aec7b88198e
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-02-09 11:06:37 +0900

    feat:課題作成機能を実装

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,5 +1,18 @@
 class SubjectsController < ApplicationController
+  before_action :set_subject, only: %i(show edit update destroy)
+
   def new
+    @subject = Subject.new
+  end
+
+  def create
+    @subject = Subject.new(subject_params)
+    if @subject.save
+      redirect_to education_path(@subject.education_id), 
+      notice: t('view.subjects.notice.create_subject')
+    else
+      render :new
+    end
   end
 
   def edit
@@ -7,4 +20,15 @@ class SubjectsController < ApplicationController
 
   def show
   end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name, :status, :deadline, :education_id)
+  end
+
+  def set_subject
+    @subject = Subject.find(params[:id])
+  end
+
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,6 +2,5 @@ class Subject < ApplicationRecord
   belongs_to :education
   validates :name, presence: true,
                   length: { maximum: 100 }
-  #boolean型のpresence: true
-  validates :status, inclusion: {in: [true, false]}
+  enum status: { "未": false, '合格': true }
 end
