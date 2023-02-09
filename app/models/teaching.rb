fb3cf0e1fcb1e686bea0741d91c2b6a4bbce7691
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-02-09 14:06:34 +0900

    feat:バリデーションを設定

--- app/models/teaching.rb
+++ app/models/teaching.rb
@@ -1,3 +1,11 @@
 class Teaching < ApplicationRecord
   belongs_to :subject
+  validates :educator, presence: true,
+                  length: { maximum: 100 }
+  validates :title, presence: true,
+                  length: { maximum: 150 }
+  validates :content, presence: true,
+                  length: { maximum: 5000 }
+  validates :advice, length: { maximum: 5000 }
+  validates :remarks, length: { maximum: 5000 }
 end
