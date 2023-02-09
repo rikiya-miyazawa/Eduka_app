896714c10eb44df1a05f9ef96fcb009f3e1be20c
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-02-09 13:34:37 +0900

    feat:ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'teachings/new'
-  get 'teachings/edit'
-  get 'teachings/show'
   resources :educations
   resources :subjects, only: %i(new create edit update show destroy)
+  resources :teachings, only: %i(new create edit update show destroy)
 end
