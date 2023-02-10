92d937cb94cb37a24374496d178d22e3fab6458c
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-02-11 08:34:31 +0900

    feat:emailにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
+  validates :email, presence: true, length: { maximum: 255 },
+          format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
