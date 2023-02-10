79d3cc0d4190b5363e24fff22d3c9a28f4d9d5f9
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-02-10 11:41:56 +0900

    feat:削除機能の実装

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,5 +1,5 @@
 class Education < ApplicationRecord
-  has_one :status
+  has_one :status, dependent: :destroy
   has_many :subjects
   validates :name, presence: true,
                   length: { maximum: 100 }
