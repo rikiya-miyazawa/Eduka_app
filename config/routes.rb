6ab9773f964fd58b24ba7b1af8f812fedda0e0da
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-02-11 08:09:06 +0900

    feat:ログイン時のルーティングを設定(仮)

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :educations
   resources :subjects, only: %i(new create edit update show destroy)
   resources :teachings, only: %i(new create edit update show destroy)
+  root 'educations#index'
 end
