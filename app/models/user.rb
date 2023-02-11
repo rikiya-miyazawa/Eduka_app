a87a6597c395c279b78b76f3016e53b34e0088e9
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-02-11 14:55:12 +0900

    feat:アソシエーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one :profile, dependent: :destroy
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
   validates :email, presence: true, length: { maximum: 255 },
           format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  accepts_nested_attributes_for :profile, allow_destroy: true, update_only: true
 end
