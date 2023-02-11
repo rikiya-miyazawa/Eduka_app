ebd8dbe92977c6da17e116870f71e8e3626deff0
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-02-11 17:21:13 +0900

    add:Teachingにuser_idを外部キーとして追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_11_081929) do
+ActiveRecord::Schema.define(version: 2023_02_11_082035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,7 +66,9 @@ ActiveRecord::Schema.define(version: 2023_02_11_081929) do
     t.bigint "subject_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["subject_id"], name: "index_teachings_on_subject_id"
+    t.index ["user_id"], name: "index_teachings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -92,4 +94,5 @@ ActiveRecord::Schema.define(version: 2023_02_11_081929) do
   add_foreign_key "subjects", "educations"
   add_foreign_key "subjects", "users"
   add_foreign_key "teachings", "subjects"
+  add_foreign_key "teachings", "users"
 end
