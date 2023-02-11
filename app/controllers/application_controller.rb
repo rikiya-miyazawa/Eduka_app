548f547c523a0591e1e09097a362f97fc9c4a62e
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-02-11 15:24:08 +0900

    feat:親子同時保存のアカウント登録画面を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [profile_attributes: [:name]])
+    devise_parameter_sanitizer.permit(:account_update, keys: [profile_attributes: [:id, :name, :hire_date, :job]])
+  end
 end
