4bfb0308af4d465ee78fbc0631b2217e300f1365
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-02-11 14:47:24 +0900

    feat:バリデーションを設定

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,6 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true,
+                  length: { maximum: 100 }
+  validates :job, length: { maximum: 100 }
 end
--- db/migrate/20230211054254_create_profiles.rb
+++ db/migrate/20230211054254_create_profiles.rb
@@ -1,7 +1,7 @@
 class CreateProfiles < ActiveRecord::Migration[6.1]
   def change
     create_table :profiles do |t|
-      t.string :name
+      t.string :name, null: false
       t.date :hire_date
       t.string :job
       t.references :user, null: false, foreign_key: true
