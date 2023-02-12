166ad0b5fb02a048b2e71dd6d480bb5ad80cdc0b
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-02-12 15:28:50 +0900

    add:ユーザの所属機能実装のためAffiliation modelを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_12_031724) do
+ActiveRecord::Schema.define(version: 2023_02_12_062622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "affiliations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "division_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["division_id"], name: "index_affiliations_on_division_id"
+    t.index ["user_id"], name: "index_affiliations_on_user_id"
+  end
 
   create_table "divisions", force: :cascade do |t|
     t.string "name", null: false
@@ -94,6 +103,8 @@ ActiveRecord::Schema.define(version: 2023_02_12_031724) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "affiliations", "divisions"
+  add_foreign_key "affiliations", "users"
   add_foreign_key "educations", "users"
   add_foreign_key "profiles", "users"
   add_foreign_key "statuses", "educations"
