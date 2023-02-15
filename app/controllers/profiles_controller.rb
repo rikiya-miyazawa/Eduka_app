3f28c3a8e7fd89de8938128a763266eb8b490fa1
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-02-15 18:24:06 +0900

    feat:ransack用のcontrollerの記述を追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,21 @@
 class ProfilesController < ApplicationController
+  before_action :set_q, only: [:index, :search]
+
   def index
     @profiles = Profile.all
   end
 
   def show
     @user = current_user
+  end
+
+  def search
+    @results = @q.result
+  end
+
+  private
+
+  def set_q
+    @q = User.ransack(params[:q])
   end
 end
