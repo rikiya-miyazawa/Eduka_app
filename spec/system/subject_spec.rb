fc62911419fdf076b0254dd56ccd3014db441a5c
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-02-24 22:06:23 +0900

    test:課題管理機能のsystem spec test実装完了

--- spec/system/subject_spec.rb
+++ spec/system/subject_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe '課題管理機能', type: :system do
     let!(:profile2) { FactoryBot.create(:second_profile, user: user2) }
     let!(:division) { FactoryBot.create(:division) }
     let!(:education) { FactoryBot.create(:education, user: user2, division: division) }
+    let!(:status) { FactoryBot.create(:status, education: education) }
     before do
       visit new_user_session_path
       fill_in 'user[email]', with: 'userspec@example.com'
@@ -17,26 +18,23 @@ RSpec.describe '課題管理機能', type: :system do
       select '下田竜也', from: 'subordinate_id'
       click_on '追加する'
       visit profiles_path
+      page.all("#education-path")[1].click
+      click_on '詳細'
     end
-    context '教育ページを作成しようとした場合' do
-      it '教育ページが作成できる' do
-        page.all("#education-path")[1].click
-        visit new_education_path(user_id: user2.id, division_id: division.id)
-        fill_in 'education[name]', with: '技術'
-        fill_in 'education[status_attributes][purpose]', with: 'お客様の信頼を得るために技術を磨こう！'
-        select '2020', from: 'education[status_attributes][created_date(1i)]'
-        select '2月', from: 'education[status_attributes][created_date(2i)]'
-        select '1', from: 'education[status_attributes][created_date(3i)]'
-        select '2022', from: 'education[status_attributes][education_period(1i)]'
-        select '2月', from: 'education[status_attributes][education_period(2i)]'
-        select '1', from: 'education[status_attributes][education_period(3i)]'
-        fill_in 'education[status_attributes][remarks]', with: 'ワインディング関東大会２位'
+    context '課題を作成しようとした場合' do
+      it '課題が作成できる' do
         click_on '作成'
-        expect(page).to have_content '教育ページを作成しました'
+        fill_in 'subject[name]', with: 'シャンプー'
+        select '合格', from: 'subject[status]'
+        select '2023', from: 'subject[deadline(1i)]'
+        select '2月', from: 'subject[deadline(2i)]'
+        select '23', from: 'subject[deadline(3i)]'
+        click_on '作成'
+        expect(page).to have_content '課題を作成しました'
       end
     end
   end
-  describe '教育ぺージ編集機能' do
+  describe '課題編集機能' do
     let!(:user1) { FactoryBot.create(:user) }
     let!(:user2) { FactoryBot.create(:second_user) }
     let!(:profile1) { FactoryBot.create(:profile, user: user1) }
@@ -44,6 +42,7 @@ RSpec.describe '課題管理機能', type: :system do
     let!(:division) { FactoryBot.create(:division) }
     let!(:education) { FactoryBot.create(:education, user: user2, division: division) }
     let!(:status) { FactoryBot.create(:status, education: education) }
+    let!(:subject) { FactoryBot.create(:subject, user: user2, education: education) }
     before do
       visit new_user_session_path
       fill_in 'user[email]', with: 'userspec@example.com'
@@ -53,27 +52,25 @@ RSpec.describe '課題管理機能', type: :system do
       select '下田竜也', from: 'subordinate_id'
       click_on '追加する'
       visit profiles_path
+      page.all("#education-path")[1].click
+      click_on '詳細'
+      sleep(2)
     end
-    context '教育ページを編集しようとした場合' do
-      it '教育ページが編集できる' do
-        page.all("#education-path")[1].click
+    context '課題を編集しようとした場合' do
+      it '課題が編集できる' do
+        click_on '詳細'
         click_on '編集'
-        sleep(5)
-        fill_in 'education[name]', with: '接客'
-        fill_in 'education[status_attributes][purpose]', with: 'お客様の信頼を得るために技術を磨こう！'
-        select '2020', from: 'education[status_attributes][created_date(1i)]'
-        select '2月', from: 'education[status_attributes][created_date(2i)]'
-        select '1', from: 'education[status_attributes][created_date(3i)]'
-        select '2022', from: 'education[status_attributes][education_period(1i)]'
-        select '2月', from: 'education[status_attributes][education_period(2i)]'
-        select '1', from: 'education[status_attributes][education_period(3i)]'
-        fill_in 'education[status_attributes][remarks]', with: 'ワインディング関東大会２位'
+        fill_in 'subject[name]', with: 'カラー'
+        select '合格', from: 'subject[status]'
+        select '2023', from: 'subject[deadline(1i)]'
+        select '2月', from: 'subject[deadline(2i)]'
+        select '23', from: 'subject[deadline(3i)]'
         click_on '更新'
-        expect(page).to have_content '接客'
+        expect(page).to have_content '課題を編集しました'
       end
     end
   end
-  describe '教育ぺージ削除機能' do
+  describe '課題削除機能' do
     let!(:user1) { FactoryBot.create(:user) }
     let!(:user2) { FactoryBot.create(:second_user) }
     let!(:profile1) { FactoryBot.create(:profile, user: user1) }
@@ -81,6 +78,7 @@ RSpec.describe '課題管理機能', type: :system do
     let!(:division) { FactoryBot.create(:division) }
     let!(:education) { FactoryBot.create(:education, user: user2, division: division) }
     let!(:status) { FactoryBot.create(:status, education: education) }
+    let!(:subject) { FactoryBot.create(:subject, user: user2, education: education) }
     before do
       visit new_user_session_path
       fill_in 'user[email]', with: 'userspec@example.com'
@@ -90,14 +88,44 @@ RSpec.describe '課題管理機能', type: :system do
       select '下田竜也', from: 'subordinate_id'
       click_on '追加する'
       visit profiles_path
+      page.all("#education-path")[1].click
+      click_on '詳細'
+      sleep(2)
     end
-    context '教育ページを削除しようとした場合' do
-      it '教育ページが削除できる' do
-        page.all("#education-path")[1].click
+    context '課題を削除しようとした場合' do
+      it '課題が削除できる' do
         page.accept_alert do
           click_on '削除'
         end
-        expect(page).to have_content '教育ページを削除しました'
+        expect(page).to have_content '課題を削除しました'
+      end
+    end
+  end
+  describe '課題合格状況確認機能' do
+    let!(:user1) { FactoryBot.create(:user) }
+    let!(:user2) { FactoryBot.create(:second_user) }
+    let!(:profile1) { FactoryBot.create(:profile, user: user1) }
+    let!(:profile2) { FactoryBot.create(:second_profile, user: user2) }
+    let!(:division) { FactoryBot.create(:division) }
+    let!(:education) { FactoryBot.create(:education, user: user2, division: division) }
+    let!(:status) { FactoryBot.create(:status, education: education) }
+    let!(:subject) { FactoryBot.create(:subject, user: user2, education: education) }
+    before do
+      visit new_user_session_path
+      fill_in 'user[email]', with: 'userspec@example.com'
+      fill_in 'user[password]', with: '111111'
+      find('#login-submit').click
+      click_on 'マイページ'
+      select '下田竜也', from: 'subordinate_id'
+      click_on '追加する'
+      visit profiles_path
+      page.all("#education-path")[1].click
+      click_on '詳細'
+      sleep(2)
+    end
+    context '教育ページで' do
+      it '課題の合格状況が確認できる' do
+        expect(page).to have_content '未'
       end
     end
   end
