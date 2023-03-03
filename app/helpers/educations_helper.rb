2aa1cd528338e3e5eb1315fc29b7c83ff3ccfaff
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-03-03 16:16:40 +0900

    refactor:superior,managerのメソッドをapplication_helperからeducations_helperへ移動

--- app/helpers/educations_helper.rb
+++ app/helpers/educations_helper.rb
@@ -1,2 +1,9 @@
 module EducationsHelper
+  def education_superior
+    @education.user.taught.ids.include?(current_user.id)
+  end
+
+  def education_manager
+    (current_user.roles.first.try(:name) == "manager" && @education.user.affiliation_divisions.exists?(id: current_user.affiliation_divisions.pluck(:id)))
+  end
 end
