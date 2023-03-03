6478bb3e9b07c15208d1bb584de5cb03155fce57
rikiya miyazawa <rky.weed.rss@gmail.com>
2023-03-03 16:27:06 +0900

    refactor:superior,managerのメソッドをapplication_helperからsubjects_helperへ移動

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,12 +14,4 @@ module ApplicationHelper
       
     end
   end
-
-  def subject_superior
-    @subject.user.taught.ids.include?(current_user.id)
-  end
-
-  def subject_manager
-    (current_user.roles.first.try(:name) == "manager" && @subject.user.affiliation_divisions.exists?(id: current_user.affiliation_divisions.pluck(:id)))
-  end
 end
